6ed873ed4f81d9048ee96b26451e579038c6ccc7
omaralveework-netizen <omaralveework@gmail.com>
2026-08-29 04:21:03 +0600

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
+import { CartProvider } from './context/CartContext'
 import Landing from './pages/Landing'
 import Admin from './pages/Admin'
 
 export default function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/admin" element={<Admin />} />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+    <CartProvider>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/admin" element={<Admin />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </CartProvider>
   )
 }
